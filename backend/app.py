44e14898b411b78591c2dbce47ecad2bf8888c19
Bryan <Bryan.wu.121@gmail.com>
2024-07-13 03:43:40 -0500

    working flask api that incorporates previous get_remotive_jobs.py

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,13 @@
-import json
-from flask import Flask, jsonify, request
+from get_remotive_jobs import return_clean_json_data
+from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/", methods=["GET"])
 def home():
-    return {"message": "success"}, 201
+    response = return_clean_json_data()
+    return {"message": response}, 200
+
+
+if __name__ == "__main__":
+    app.run(port=8080)
