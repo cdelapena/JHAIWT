0af27620d4ab68147d4d30e1e3c88e4b73fe908d
Chris Dela Pena <delapenc@oregonstate.edu>
2024-08-05 01:19:27 -0700

    Revising capitalization of navbar in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,7 +15,7 @@ import Home from "./Pages/Home/Home";
 import ResultsPage from "./Pages/ResultsPage/ResultsPage";
 import BrowsePage from "./Pages/BrowsePage/BrowsePage";
 import { getDesignTokens } from "./shared/colorTheme";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import { initialSearchValues } from "./shared/constants";
 import { SearchInterface } from "./shared/interfaces";
 
